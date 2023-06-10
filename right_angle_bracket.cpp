f83908863e7d722fdd95a7177bbf363c4da39d20
Abhishek <abhibuddyofficial@gmail.com>
2023-06-10 20:55:19 +0530

    right angle bracket

---
+++ right_angle_bracket.cpp
@@ -0,0 +1,14 @@
+/*
+before the c++11 version we can't use >> other than that for cin 
+as it was leading to confusion
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    std::vector<std::vector<int>> matrix;
+    return 0;
+}
