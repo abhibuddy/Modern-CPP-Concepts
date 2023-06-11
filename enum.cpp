86b8c52fae99e80725a3a6f467e1762c38eb18fa
Abhishek <abhibuddyofficial@gmail.com>
2023-06-11 10:27:38 +0530

    used of enum class

---
+++ enum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+enum class Color 
+{
+Red,Green,Blue
+};
+int main()
+{
+    Color c = Color::Green;
+    if(c == Color::Red)
+    {
+        std::cout<<"colur is Red"<<std::endl;
+    }
+    else if (c == Color::Green)
+    {
+        std::cout<<"colur is Green"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"colur is Blue"<<std::endl;
+    }
+    return 0;
+}
