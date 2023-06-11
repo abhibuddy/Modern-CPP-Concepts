1de5f25779f6e92ab851589faf513c9723495529
Abhishek <abhibuddyofficial@gmail.com>
2023-06-11 14:33:55 +0530

    use of nullptr

---
+++ nullptr.cpp
@@ -0,0 +1,15 @@
+/*nullptr*/
+#include<iostream>
+using namespace std;
+//void fun(int){}; // not convertable to int except bool
+void fun(int* p){
+    if(p==nullptr)
+    {
+        std::cout<<"nullptr"<<std::endl;
+    }
+}
+int main()
+{
+	fun(nullptr);  //can be implicitely converted pointer type 
+ return 0;
+}
