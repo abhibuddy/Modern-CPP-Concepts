ed0593bb22d0f4b38167eadca5beaa702ccb30dd
Abhishek <abhibuddyofficial@gmail.com>
2023-06-11 14:32:25 +0530

    std::array container

---
+++ array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+
+    array<int,5> arr= {1,2,3,4,5};
+    for(auto& i:arr)    // call by reference
+    {   i=i*2;
+        std::cout<<i<<" ";
+    }
+    std::cout<<std::endl;
+    for(auto i:arr)    //call by value
+    {
+        std::cout<< i <<" ";
+    }
+    std::cout<<std::endl;
+    return 0;
+}
