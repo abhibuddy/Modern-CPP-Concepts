77633a19f03d53cda9ea7db6c6778df4cca36c61
Abhishek <abhibuddyofficial@gmail.com>
2023-06-11 14:34:33 +0530

    use of to_string

---
+++ to_string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+    //to_string converts any type to string 
+    std::cout<<"concate the value of pi at the end of string: "<<std::to_string(3.14)<<std::endl;
+    return 0;
+}
