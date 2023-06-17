93cd252d5be0da48c7ecde9d5a4583420a5d54cf
Abhishek <abhibuddyofficial@gmail.com>
2023-06-17 11:10:33 +0530

    use of function

---
+++ function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <functional>
+
+using namespace std;
+int add(int a, int b)
+{
+    return a+b;
+}
+struct mlt
+{
+    int operator()(int a, int b) const
+    {
+        return a*b;
+    }
+};
+int main()
+{
+    std::function<int(int,int)> funct1 = add;
+    std::function<int(int,int)> funct2 = mlt();
+
+    std::cout<<funct1(5,6)<<std::endl;
+    std::cout<<funct2(5,6);
+    return 0;
+}
