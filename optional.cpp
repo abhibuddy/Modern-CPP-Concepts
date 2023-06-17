6928bef52436506c0568b7d5891d3172ab26ab61
Abhishek <abhibuddyofficial@gmail.com>
2023-06-17 11:18:25 +0530

    use of optional

---
+++ optional.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <optional>
+using namespace std;
+
+std::optional<int> devide(int a, int b)
+{
+if(a!=0)
+{
+    return b/a;
+}
+else {
+    return nullopt;
+}
+
+}
+int main()
+{
+    std::optional<int> result = devide(0,15);
+    if(result.has_value())
+    {
+        std::cout<<result.value()<<std::endl;
+    }
+    else
+    {
+        std::cout<<"can't divide with 0"<<std::endl;
+    }
+    return 0;
+}
