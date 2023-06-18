9f08988b8c5dd3f8b88cebb44c19c6d6ee797bb5
Abhishek <abhibuddyofficial@gmail.com>
2023-06-18 17:59:47 +0530

    use of digit seperator in modern cpp

---
+++ digit_seperation.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //single quotes (') can be used as comma seperator 
+    int money = 7'900'000; 
+    std::cout<<money<<std::endl;
+    return 0;
+}
